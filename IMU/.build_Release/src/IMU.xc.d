d6578532bbace9c3bf36c2c18a71226432df3e1b
Phani Gaddipati <phani.gaddipati@centumengineering.com>
2012-05-28 15:09:00 -0600

    Build files

--- IMU/.build_Release/src/IMU.xc.d
+++ IMU/.build_Release/src/IMU.xc.d
@@ -4,6 +4,7 @@
   C:/Users/Phani/Documents/GitHub/XMOS_IMU/IMU/src/Libs/UART/RX/uart_rx_impl.h \
   C:/Users/Phani/Documents/GitHub/XMOS_IMU/IMU/src/Sensors/GPS.h \
   C:/Users/Phani/Documents/GitHub/XMOS_IMU/IMU/src/Sensors/Magnetometer.h \
-  C:/Users/Phani/Documents/GitHub/XMOS_IMU/IMU/src/Libs/I2C/i2c.h
+  C:/Users/Phani/Documents/GitHub/XMOS_IMU/IMU/src/Libs/I2C/i2c.h \
+  C:/Users/Phani/Documents/GitHub/XMOS_IMU/IMU/src/Sensors/GPS_Funcs.h
 
 
